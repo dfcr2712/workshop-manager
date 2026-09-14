5f14e49827a768e9bde565b9d66519dadca4bc3e
Daniel Rodrigues <dfcr2712@gmail.com>
2026-09-14 22:28:20 +0100

    Connect customer creation to backend

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,88 +1,35 @@
 "use strict";
 
-let nextCustomerId = 2;
 let editingCustomerId = null;
 let editingRow = null;
 
 const customerTable = document.querySelector(".customer-table");
-
-customerTable.addEventListener("click", (event) => {
-    const editButton = event.target.closest(".edit-button");
-    const deleteButton = event.target.closest(".delete-button");
-
-    if (editButton) {
-        const customerId = editButton.dataset.customerId;
-
-        editingCustomerId = customerId;
-
-        console.log("Edit: ", customerId);
-
-        editingRow = editButton.closest("tr");
-        const cells = editingRow.querySelectorAll("td");
-
-        nameInput.value = cells[0].textContent;
-        emailInput.value = cells[1].textContent;
-        phoneInput.value = cells[2].textContent;
-
-        return;
-    }
-
-    if (deleteButton) {
-        const customerId = deleteButton.dataset.customerId;
-        const row = deleteButton.closest("tr");
-        row.remove();
-
-        console.log("Delete: ", customerId);
-    }
-});
-
-
 const customerForm = document.querySelector("#customer-form");
-
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const phoneInput = document.querySelector("#phone");
-
+const nifInput = document.querySelector("#nif");
 const customerTableBody = document.querySelector(".customer-table tbody");
 
-customerForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+// Função que recebe um cliente e desenha uma linha na tabela
 
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const phone = phoneInput.value;
-
-    if(editingCustomerId !== null) {
-        const cells = editingRow.querySelectorAll("td");
-        cells[0].textContent = name;
-        cells[1].textContent = email;
-        cells[2].textContent = phone;
-
-        editingCustomerId = null;
-        editingRow = null;
-
-        customerForm.reset();
-
-        return;
-    }
-
-    const customer = {
-        id: nextCustomerId, name, email, phone
-    };
-    nextCustomerId++;
-
+function addCustomerToTable(customer) {
     const row = document.createElement("tr");
 
     const nameCell = document.createElement("td");
     nameCell.textContent = customer.name;
     row.appendChild(nameCell);
 
+    const nifCell = document.createElement("td");
+    nifCell.textContent = customer.nif;
+    row.appendChild(nifCell);
+
     const emailCell = document.createElement("td");
     emailCell.textContent = customer.email;
     row.appendChild(emailCell);
 
     const phoneCell = document.createElement("td");
-    phoneCell.textContent = customer.phone;
+    phoneCell.textContent = customer.phoneNumber;
     row.appendChild(phoneCell);
 
     // ---------------
@@ -101,11 +48,111 @@ customerForm.addEventListener("submit", (event) => {
     deleteButton.classList.add("action-button", "delete-button");
     deleteButton.dataset.customerId = String(customer.id);
 
-
     actionsCell.appendChild(editButton);
     actionsCell.appendChild(deleteButton);
     row.appendChild(actionsCell);
 
     customerTableBody.appendChild(row);
-    console.log(customer);
-})
+}
+
+
+// Função que vai ao backend buscar os clientes que estão na base de dados
+// “Para cada customer que veio da base de dados, adiciona esse customer à tabela.”
+
+async function loadCustomers() {
+    const response = await fetch("/customers");
+    const customers = await response.json();
+
+    customers.forEach(customer => {
+        addCustomerToTable(customer);
+    });
+}
+
+
+async function createCustomer(customerData){
+    const response = await fetch("/customers", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customerData)
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error creating customer: ${response.status}`);
+    }
+    const createdCustomer = await response.json();
+
+    return createdCustomer;
+}
+
+customerTable.addEventListener("click", (event) => {
+    const editButton = event.target.closest(".edit-button");
+    const deleteButton = event.target.closest(".delete-button");
+
+    if (editButton) {
+        const customerId = editButton.dataset.customerId;
+
+        editingCustomerId = customerId;
+
+        console.log("Edit: ", customerId);
+
+        editingRow = editButton.closest("tr");
+        const cells = editingRow.querySelectorAll("td");
+
+        nameInput.value = cells[0].textContent;
+        nifInput.value = cells[1].textContent;
+        emailInput.value = cells[2].textContent;
+        phoneInput.value = cells[3].textContent;
+
+        return;
+    }
+
+    if (deleteButton) {
+        const customerId = deleteButton.dataset.customerId;
+        const row = deleteButton.closest("tr");
+        row.remove();
+
+        console.log("Delete: ", customerId);
+    }
+});
+
+// SUBMIT
+customerForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const phone = phoneInput.value;
+    const nif = nifInput.value;
+
+    if (editingCustomerId !== null) {
+        const cells = editingRow.querySelectorAll("td");
+        cells[0].textContent = name;
+        cells[1].textContent = nif;
+        cells[2].textContent = email;
+        cells[3].textContent = phone;
+
+        editingCustomerId = null;
+        editingRow = null;
+
+        customerForm.reset();
+
+        return;
+    }
+
+    const customerData = {
+        name,
+        nif,
+        phoneNumber: phone,
+        email
+    };
+
+    const createdCustomer = await createCustomer(customerData);
+
+    addCustomerToTable(createdCustomer);
+
+    customerForm.reset();
+});
+
+loadCustomers();
